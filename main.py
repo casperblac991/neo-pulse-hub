cdf6bd8b8e113fb1d76fef66e610fd6ffc95de0b
casperblac991 <casperblac099@gmail.com>
2026-03-08 15:20:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -253,3 +253,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+    import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'bots'))
+from customer_bot import main
+main()
